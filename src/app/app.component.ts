2f95ef6e76514c16c8a529f281b2852a8f18f1f5
Ritesh <arora.ritesh.95@gmail.com>
2022-01-07 18:22:53 +0530

    add temporary solution to navigatiob

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  one:boolean=false;
+  setValue(event:{ans:string}){
+    console.log(event);
+    if(event.ans=='recipe'){
+      this.one=true;
+    }
+    else{
+      this.one=false;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html',
 })
 export class HeaderComponent{
-
+@Output() answer=new EventEmitter<{ans:string}>();
+onRecipe(){
+    this.answer.emit({ans:'recipe'});
+}
+onShop(){
+    this.answer.emit({ans:'shop'});
+}
 }
