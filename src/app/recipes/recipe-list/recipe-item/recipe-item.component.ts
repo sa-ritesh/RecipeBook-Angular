54bf38da89c089149b9b213a147ead9e66e8ba7b
Ritesh <arora.ritesh.95@gmail.com>
2022-01-07 18:44:16 +0530

    recipe list breakdown

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
   constructor() { }
+  @Input() recipe:Recipe=new Recipe('','','');
 
   ngOnInit(): void {
   }
