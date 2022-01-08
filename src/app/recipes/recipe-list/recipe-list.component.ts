fc9304bbb424016e814050fd91bec5428ed9f90e
Ritesh <arora.ritesh.95@gmail.com>
2022-01-08 14:39:51 +0530

    managing recipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,16 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes:Recipe[]=[
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
   @Output() recipeWasSelected=new EventEmitter<Recipe>();
-  constructor() { }
+  recipes:Recipe[]=[];
+  constructor(private recipeService:RecipeService) { }
   onRecipeSelected(recipe:Recipe){
     this.recipeWasSelected.emit(recipe);
   }
   ngOnInit(): void {
+    this.recipes=this.recipeService.getRecipesMethod();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
-export class RecipeService{
+import { Recipe } from "./recipe.model";
 
+export class RecipeService{
+   private recipes:Recipe[]=[
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+      ];
+    getRecipesMethod(){
+        return this.recipes.slice();
+    }
 }
