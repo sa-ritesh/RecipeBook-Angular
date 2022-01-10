e768b38281ea353b2155ba540b2027e40adb57a4
Ritesh <arora.ritesh.95@gmail.com>
2022-01-10 10:14:04 +0530

    sl optimesd with observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -7,7 +8,7 @@ export class RecipeService{
     constructor(private slService:ShoppingListService){
 
     }
-   recipeSelected=new EventEmitter<Recipe>();
+   recipeSelected=new Subject<Recipe>();
    private recipes:Recipe[]=[
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',[new Ingredient('Meat',1), new Ingredient('Feet',1)]),
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',[new Ingredient('Meat',1), new Ingredient('Feet',1)])
