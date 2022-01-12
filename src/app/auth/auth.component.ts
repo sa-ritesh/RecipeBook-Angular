033f5b13216963a87813def27075eade8824230d
Ritesh <arora.ritesh.95@gmail.com>
2022-01-12 22:12:01 +0530

    switch login and signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,4 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+    isLoggedIn:boolean=false;
+    onSwitch(){
+       this.isLoggedIn=!this.isLoggedIn;
+    }
+}
