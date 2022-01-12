8c324a61e3cd70aeb73f9503ccc15de5af5bcc6b
Ritesh <arora.ritesh.95@gmail.com>
2022-01-12 20:28:06 +0530

    add ingredient bug fix

--- src/app/recipes/data-storabe.service.ts
+++ src/app/recipes/data-storabe.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 import { Recipe } from "./recipe.model";
 import { RecipeService } from "./recipe.service";
 
@@ -17,6 +18,11 @@ export class DataStorageService{
 
    fetchRecipes(){
       this.http.get<Recipe[]>('https://recipe-angular-2e834-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map(recipes=>{
+            return recipes.map(recipe=>{
+               return {...recipe,ingredients:recipe.ingredients?recipe.ingredients:[]};
+            })
+      }))
       .subscribe((recipes)=>{
          //  console.log(response);
          this.recipeService.setRecipes(recipes);
