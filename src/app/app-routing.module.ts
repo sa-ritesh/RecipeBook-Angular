d1e7db93406671e5fd74173cb0c7c4295dfdd600
Ritesh <arora.ritesh.95@gmail.com>
2022-01-13 17:41:46 +0530

    auth gaurd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.gaurd";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
@@ -11,7 +12,8 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes:Routes=[
     {path:'',redirectTo:'/recipes',pathMatch:'full'},
-    {path:'recipes',component:RecipesComponent, children:[
+    {path:'recipes',component:RecipesComponent, canActivate:[AuthGuard],
+     children:[
         {path:'',component:RecipeStartComponent},
         {path:'new',component:RecipeEditComponent},
         {path:':id',component:RecipeDetailComponent,resolve:[RecipesResolverService]},
